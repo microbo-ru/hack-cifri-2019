4515a8b4420b8f8e9d13a0ee7e4f4605e3c0afdd
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2019-06-09 10:23:10 +0300

    clean up

--- get_color.js
+++ get_color.js
@@ -1,18 +1,18 @@
 var heatmap = {}
 $.getJSON("heatmap.json", function(json) {
   heatmap = json;
-
 });
 
 var get_color = function(t) {
   var key = t.properties.name;
+  print(key);
   if (key in heatmap) {
     return heatmap[key];
   } else {
     if ("default" in heatmap) {
       return heatmap["default"];
     } else {
-      return '#FFE2';
+      return "#FFF";
     }
   }
 };
